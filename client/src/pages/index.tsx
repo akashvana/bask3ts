2a4c2d33bc47b4d15d9f55191f6945dbd7ef8257
akashvana <akashvana2001@gmail.com>
2023-12-09 22:40:02 +0530

    temporary aa-integration

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Mywallet from './wallet'
+// import Mywallet from './wallet'
+import MyWallet from '../components/CreateSmartAccount'
 import Header from '@/components/Header'
 import 'bootstrap/dist/css/bootstrap.css'
 import firstpage from '../pages/public/firstpage.png'
@@ -15,7 +16,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <Header/>
-       <Mywallet />
+       <MyWallet />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <div className="container text-center">
@@ -28,7 +29,7 @@ export default function Home() {
     <div className="col-8">
     You don’t have to be a digital marketing pro to know how important a website is to modern business. As both a digital interface for delivering products and services and a vehicle for generating leads, your website needs to look good. If you want to deliver a smooth customer experience and look good while doing it, a web designer can help.
     <br />
-    <Link href="/buckets" classNameName="hover:text-blue-200">
+    <Link href="/buckets" className="hover:text-blue-200">
     <button type="button" className="btn btn-primary">Launch</button>
     </Link>
     </div>
